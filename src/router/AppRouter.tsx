eedcc2e0916c11f5386326f0246668aee81a87e6
imalexxx14 <1116763@est.intec.edu.do>
2026-04-25 19:06:35 -0400

    feat: add pages for managing institutions, document types, and fee assignments in the admin dashboard

--- src/router/AppRouter.tsx
+++ src/router/AppRouter.tsx
@@ -22,6 +22,8 @@ import { SolicitudDetailPage } from '../modules/ciudadano/pages/MiSolicitudDetai
 import { PaymentPage } from '../modules/ciudadano/pages/PaymentPage.tsx';
 import { AdminDashboardPage } from '../modules/Admin/pages/AdminDashboardPage';
 import { BulkUploadPage } from '../modules/Admin/pages/BulkUploadPage';
+import { CreateInstitucionPage } from '../modules/Admin/pages/CreateInstitucionPage';
+import { CreateDocumentTypePage } from '../modules/Admin/pages/CreateDocumentTypePage';
 import { FormulariosPage } from '../modules/Admin/pages/Formularios/FormulariosPage.tsx';
 import { CreateFormularioPage } from '../modules/Admin/pages/Formularios/CreateFormulariosPage.tsx';
 import { EditFormularioPage } from '../modules/Admin/pages/Formularios/EditFormularioPage.tsx';
@@ -66,6 +68,9 @@ export const AppRouter = () => {
         {/* Admin */}
         <Route path="/admin" element={<AdminDashboardPage />} />
         <Route path="/admin/carga-masiva" element={<BulkUploadPage />} />
+        <Route path="/admin/instituciones" element={<CreateInstitucionPage />} />
+        <Route path="/admin/tipos-documento" element={<CreateDocumentTypePage />} />
+
         <Route path="/admin/formularios" element={<FormulariosPage />} />
         <Route path="/admin/formularios/create" element={<CreateFormularioPage />} />
         <Route path="/admin/formularios/edit/:id" element={<EditFormularioPage />} />
